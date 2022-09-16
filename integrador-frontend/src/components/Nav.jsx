1d29c0b6682cda5a255aa9663f0d6ee76e8e50ca
Vinícius Marques <58312849+marqvin@users.noreply.github.com>
2022-09-16 01:44:59 -0300

    deixando Nav responsiva

--- integrador-frontend/src/components/Nav.jsx
+++ integrador-frontend/src/components/Nav.jsx
@@ -3,12 +3,14 @@ import {Link} from 'react-router-dom'
 function Nav(){
     return(
         
-        <nav>
-            <Link to='/'>Home  </Link>
-            <Link to='/product'>Produtos  </Link>
-            <Link to='/shoppingcart'>Carrinho  </Link>
-            <Link to='/management'>Administração  </Link>
-            <Link to='/aboutus'>Sobre Nós  </Link>
+        <nav class="navbar navbar-expand-lg bg-light">
+            <div class="container-fluid">
+                <Link to='/' >Home  </Link>
+                <Link to='/product' >Produtos  </Link>
+                <Link to='/shoppingcart' >Carrinho  </Link>
+                <Link to='/management' >Administração  </Link>
+                <Link to='/aboutus' >Sobre Nós  </Link>
+            </div>
         </nav>
     )
 }
