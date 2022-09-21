d6c7da5b4653ffd5df52fff78e6d1d363763ac70
Vinícius Marques <58312849+marqvin@users.noreply.github.com>
2022-09-21 03:00:17 -0300

    Estilizando a Nav
    Co-Authored-By: jhonatan <99609730+spark2617@users.noreply.github.com>

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,15 +1,15 @@
 import {Link} from 'react-router-dom'
-
+import 'react-bootstrap/Badge';
 function Nav(){
     return(
         
         <nav class="navbar navbar-expand-lg bg-light">
             <div class="container-fluid">
-                <Link to='/' >Home  </Link>
-                <Link to='/product' >Produtos  </Link>
-                <Link to='/shoppingcart' >Carrinho  </Link>
-                <Link to='/management' >Administração  </Link>
-                <Link to='/aboutus' >Sobre Nós  </Link>
+                <Link to='/' class="text-decoration-none">Home  </Link>
+                <Link to='/product' class="text-decoration-none">Produtos  </Link>
+                <Link to='/shoppingcart' class="text-decoration-none">Carrinho  </Link>
+                <Link to='/management' class="text-decoration-none">Administração  </Link>
+                <Link to='/aboutus' class="text-decoration-none">Sobre Nós  </Link>
             </div>
         </nav>
     )
