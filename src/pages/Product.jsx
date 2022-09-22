fc2b60b6a4a427b798643eb75617087461283d08
Vinícius Marques <58312849+marqvin@users.noreply.github.com>
2022-09-21 21:21:44 -0300

    Estruturando Product

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,7 +1,298 @@
-function Product(){
-    return(
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import Figure from 'react-bootstrap/Figure';
+
+
+function Product() {
+
+    return (
         <>
-            <h1>Produtos</h1>
+
+            <Container fluid>
+                <Row className="justify-content-md-center">
+                    <Col>
+                        <Figure>
+                            <Figure.Image
+                                width={264.03}
+                                height={264.6}
+                                alt="171x180"
+                                src="https://st.depositphotos.com/2935381/4189/i/450/depositphotos_41897159-stock-photo-example-concept.jpg"
+                            />
+                            <Figure.Caption>
+                                <Row>
+                                <Col>
+                                    <h5>Kabum</h5>
+                                </Col>
+                                <Col>
+                                    ⭐⭐⭐⭐⭐
+                                </Col>
+                                </Row>
+                                <p>R$ X Bilhões</p>
+                                
+
+                                Nulla vitae elit libero, a pharetra augue mollis interdum.
+                            </Figure.Caption>
+                        </Figure>
+                    </Col>
+                    <Col>
+                        <Figure>
+                            <Figure.Image
+                                width={264.03}
+                                height={264.6}
+                                alt="171x180"
+                                src="https://st.depositphotos.com/2935381/4189/i/450/depositphotos_41897159-stock-photo-example-concept.jpg"
+                            />
+                            <Figure.Caption>
+                            <Row>
+                                <Col>
+                                    <h5>Kabum</h5>
+                                </Col>
+                                <Col>
+                                    ⭐⭐⭐⭐⭐
+                                </Col>
+                                </Row>
+                                <p>R$ X Bilhões</p>
+                                Nulla vitae elit libero, a pharetra augue mollis interdum.
+                            
+                                </Figure.Caption>
+                        </Figure>
+                    </Col>
+                    <Col>
+                        <Figure>
+                            <Figure.Image
+                                width={264.03}
+                                height={264.6}
+                                alt="171x180"
+                                src="https://st.depositphotos.com/2935381/4189/i/450/depositphotos_41897159-stock-photo-example-concept.jpg"
+                            />
+                            <Figure.Caption>
+                            <Row>
+                                <Col>
+                                    <h5>Kabum</h5>
+                                </Col>
+                                <Col>
+                                    ⭐⭐⭐⭐⭐
+                                </Col>
+                                </Row>
+                                <p>R$ X Bilhões</p>
+                                Nulla vitae elit libero, a pharetra augue mollis interdum.
+                            </Figure.Caption>
+                        </Figure>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Figure>
+                            <Figure.Image
+                                width={264.03}
+                                height={264.6}
+                                alt="171x180"
+                                src="https://st.depositphotos.com/2935381/4189/i/450/depositphotos_41897159-stock-photo-example-concept.jpg"
+                            />
+                            <Figure.Caption>
+                            <Row>
+                                <Col>
+                                    <h5>Kabum</h5>
+                                </Col>
+                                <Col>
+                                    ⭐⭐⭐⭐⭐
+                                </Col>
+                                </Row>
+                                <p>R$ X Bilhões</p>
+                                Nulla vitae elit libero, a pharetra augue mollis interdum.
+                            </Figure.Caption>
+                        </Figure>
+                    </Col>
+                    <Col>
+                        <Figure>
+                            <Figure.Image
+                                width={264.03}
+                                height={264.6}
+                                alt="171x180"
+                                src="https://st.depositphotos.com/2935381/4189/i/450/depositphotos_41897159-stock-photo-example-concept.jpg"
+                            />
+                            <Figure.Caption>
+                            <Row>
+                                <Col>
+                                    <h5>Kabum</h5>
+                                </Col>
+                                <Col>
+                                    ⭐⭐⭐⭐⭐
+                                </Col>
+                                </Row>
+                                <p>R$ X Bilhões</p>
+                                Nulla vitae elit libero, a pharetra augue mollis interdum.
+                            </Figure.Caption>
+                        </Figure>
+                    </Col>
+                    <Col>
+                        <Figure>
+                            <Figure.Image
+                                width={264.03}
+                                height={264.6}
+                                alt="171x180"
+                                src="https://st.depositphotos.com/2935381/4189/i/450/depositphotos_41897159-stock-photo-example-concept.jpg"
+                            />
+                            <Figure.Caption>
+                            <Row>
+                                <Col>
+                                    <h5>Kabum</h5>
+                                </Col>
+                                <Col>
+                                    ⭐⭐⭐⭐⭐
+                                </Col>
+                                </Row>
+                                <p>R$ X Bilhões</p>
+                                Nulla vitae elit libero, a pharetra augue mollis interdum.
+                            </Figure.Caption>
+                        </Figure>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Figure>
+                            <Figure.Image
+                                width={264.03}
+                                height={264.6}
+                                alt="171x180"
+                                src="https://st.depositphotos.com/2935381/4189/i/450/depositphotos_41897159-stock-photo-example-concept.jpg"
+                            />
+                            <Figure.Caption>
+                                <Row>
+                                <Col>
+                                    <h5>Kabum</h5>
+                                </Col>
+                                <Col>
+                                    ⭐⭐⭐⭐⭐
+                                </Col>
+                                </Row>
+                                <p>R$ X Bilhões</p>
+                                
+
+                                Nulla vitae elit libero, a pharetra augue mollis interdum.
+                            </Figure.Caption>
+                        </Figure>
+                    </Col>
+                    <Col>
+                        <Figure>
+                            <Figure.Image
+                                width={264.03}
+                                height={264.6}
+                                alt="171x180"
+                                src="https://st.depositphotos.com/2935381/4189/i/450/depositphotos_41897159-stock-photo-example-concept.jpg"
+                            />
+                            <Figure.Caption>
+                            <Row>
+                                <Col>
+                                    <h5>Kabum</h5>
+                                </Col>
+                                <Col>
+                                    ⭐⭐⭐⭐⭐
+                                </Col>
+                                </Row>
+                                <p>R$ X Bilhões</p>
+                                Nulla vitae elit libero, a pharetra augue mollis interdum.
+                            
+                                </Figure.Caption>
+                        </Figure>
+                    </Col>
+                    <Col>
+                        <Figure>
+                            <Figure.Image
+                                width={264.03}
+                                height={264.6}
+                                alt="171x180"
+                                src="https://st.depositphotos.com/2935381/4189/i/450/depositphotos_41897159-stock-photo-example-concept.jpg"
+                            />
+                            <Figure.Caption>
+                            <Row>
+                                <Col>
+                                    <h5>Kabum</h5>
+                                </Col>
+                                <Col>
+                                    ⭐⭐⭐⭐⭐
+                                </Col>
+                                </Row>
+                                <p>R$ X Bilhões</p>
+                                Nulla vitae elit libero, a pharetra augue mollis interdum.
+                            </Figure.Caption>
+                        </Figure>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Figure>
+                            <Figure.Image
+                                width={264.03}
+                                height={264.6}
+                                alt="171x180"
+                                src="https://st.depositphotos.com/2935381/4189/i/450/depositphotos_41897159-stock-photo-example-concept.jpg"
+                            />
+                            <Figure.Caption>
+                            <Row>
+                                <Col>
+                                    <h5>Kabum</h5>
+                                </Col>
+                                <Col>
+                                    ⭐⭐⭐⭐⭐
+                                </Col>
+                                </Row>
+                                <p>R$ X Bilhões</p>
+                                Nulla vitae elit libero, a pharetra augue mollis interdum.
+                            </Figure.Caption>
+                        </Figure>
+                    </Col>
+                    <Col>
+                        <Figure>
+                            <Figure.Image
+                                width={264.03}
+                                height={264.6}
+                                alt="171x180"
+                                src="https://st.depositphotos.com/2935381/4189/i/450/depositphotos_41897159-stock-photo-example-concept.jpg"
+                            />
+                            <Figure.Caption>
+                            <Row>
+                                <Col>
+                                    <h5>Kabum</h5>
+                                </Col>
+                                <Col>
+                                    ⭐⭐⭐⭐⭐
+                                </Col>
+                                </Row>
+                                <p>R$ X Bilhões</p>
+                                Nulla vitae elit libero, a pharetra augue mollis interdum.
+                            </Figure.Caption>
+                        </Figure>
+                    </Col>
+                    <Col>
+                        <Figure>
+                            <Figure.Image
+                                width={264.03}
+                                height={264.6}
+                                alt="171x180"
+                                src="https://st.depositphotos.com/2935381/4189/i/450/depositphotos_41897159-stock-photo-example-concept.jpg"
+                            />
+                            <Figure.Caption>
+                            <Row>
+                                <Col>
+                                    <h5>Kabum</h5>
+                                </Col>
+                                <Col>
+                                    ⭐⭐⭐⭐⭐
+                                </Col>
+                                </Row>
+                                <p>R$ X Bilhões</p>
+                                Nulla vitae elit libero, a pharetra augue mollis interdum.
+                            </Figure.Caption>
+                        </Figure>
+                    </Col>
+                </Row>
+            </Container>
         </>
     )
 }
